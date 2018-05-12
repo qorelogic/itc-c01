0fba2cd685e2f6d1d20e591f8c61c9ef40b5dad4
Jeff Kiiza <kiizaa@gmail.com>
2018-05-12 00:43:46 -0300

    models.py: users, todo

--- movie/models.py
+++ movie/models.py
@@ -1,3 +1,18 @@
+
 from django.db import models
 
 # Create your models here.
+from users.models import User
+ 
+class Movie(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+
+    title = models.CharField(max_length=100, unique=True, blank=False, null=False)
+    releaseYear = models.CharField(max_length=100, unique=True, blank=False, null=False, default='N/A')
+    casting = models.CharField(max_length=100, unique=True, blank=False, null=False, default='N/A')
+    directors = models.CharField(max_length=100, unique=True, blank=False, null=False, default='N/A')
+    producers = models.CharField(max_length=100, unique=True, blank=False, null=False, default='N/A')
+
+    user = models.ForeignKey('users.User', related_name='movie', on_delete=models.CASCADE, null=False)
+    class Meta:
+        ordering = ('created',)
--- person/models.py
+++ person/models.py
@@ -1,3 +1,20 @@
+
 from django.db import models
 
 # Create your models here.
+from users.models import User
+ 
+class Person(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+
+    firstName = models.CharField(max_length=100, unique=True, blank=False, null=False)
+    lastName = models.CharField(max_length=100, unique=True, blank=False, null=False, default='N/A')
+    aliases = models.CharField(max_length=100, unique=True, blank=False, null=False, default='N/A')
+    moviesAsActorActress = models.CharField(max_length=100, unique=True, blank=False, null=False, default='N/A')
+    moviesAsDirector = models.CharField(max_length=100, unique=True, blank=False, null=False, default='N/A')
+    moviesAsProducer = models.CharField(max_length=100, unique=True, blank=False, null=False, default='N/A')
+
+    user = models.ForeignKey('users.User', related_name='person', on_delete=models.CASCADE, null=False)
+
+    class Meta:
+        ordering = ('created',)
