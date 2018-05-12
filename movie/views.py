2dc368cd9689f58cf33da1167ecb0666d392b4b7
Jeff Kiiza <kiizaa@gmail.com>
2018-05-12 00:43:00 -0300

    views.py: person, movie

--- movie/views.py
+++ movie/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
 # Create your views here.
+from movie.models import Movie
+from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
+ 
+from movie.serializers import MovieSerializer
+ 
+class MovieList(generics.ListCreateAPIView):
+    queryset = Movie.objects.all()
+    serializer_class = MovieSerializer
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+class MovieDetail(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = MovieSerializer
+    def get_queryset(self):
+        return Movie.objects.all().filter(user=self.request.user)
+
--- person/views.py
+++ person/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
 # Create your views here.
+from person.models import Person
+from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
+ 
+from person.serializers import PersonSerializer
+ 
+class PersonList(generics.ListCreateAPIView):
+    queryset = Person.objects.all()
+    serializer_class = PersonSerializer
+ 
+class PersonDetail(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = PersonSerializer
+ 
+    def get_queryset(self):
+        return Person.objects.all().filter(username=self.request.user)
+
